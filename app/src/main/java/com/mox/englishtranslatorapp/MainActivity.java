a299569add0fd2b888eedc3918ef411c946fa1da
Maciej <maciek.krupinski30@gmail.com>
2022-03-02 14:25:20 +0100

    First Commit

---
+++ app/src/main/java/com/mox/englishtranslatorapp/MainActivity.java
@@ -0,0 +1,168 @@
+package com.mox.englishtranslatorapp;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.speech.RecognizerIntent;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.mlkit.common.model.DownloadConditions;
+import com.google.mlkit.nl.translate.TranslateLanguage;
+import com.google.mlkit.nl.translate.Translation;
+import com.google.mlkit.nl.translate.Translator;
+import com.google.mlkit.nl.translate.TranslatorOptions;
+
+import java.util.ArrayList;
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Spinner fromSpinner, toSpinner;
+    private TextInputEditText sourceText;
+    private ImageView micIV;
+    private MaterialButton translateBtn;
+    private TextView translateTV;
+    private ImageView exChange;
+    String[] fromLanguage = {"Angielski", "Polski"};
+    String[] toLanguage = {"Polski", "Angielski"};
+    private static final int REQUEST_PERMISSION_CODE = 1;
+    int languageCode, fromLanguageCode, toLanguageCode = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        fromSpinner = findViewById(R.id.idFromSpinner);
+        toSpinner = findViewById(R.id.idToSpinner);
+        sourceText = findViewById(R.id.idEditSource);
+        micIV = findViewById(R.id.idIVMic);
+        translateBtn = findViewById(R.id.idBtnTranslation);
+        translateTV = findViewById(R.id.idTranslatedTV);
+        exChange = findViewById(R.id.idExChange);
+        fromSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                fromLanguageCode = getLanguageCode(fromLanguage[i]);
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+            }
+        });
+        ArrayAdapter fromAdapter = new ArrayAdapter(this, R.layout.spinner_item, fromLanguage);
+        fromAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        fromSpinner.setAdapter(fromAdapter);
+        toSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                toLanguageCode = getLanguageCode(toLanguage[i]);
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+            }
+        });
+        ArrayAdapter toAdapter = new ArrayAdapter(this, R.layout.spinner_item, toLanguage);
+        toAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        toSpinner.setAdapter(toAdapter);
+
+        exChange.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        micIV.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+                intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+                intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+                intent.putExtra(RecognizerIntent.EXTRA_PROMPT,"Mów teraz...");
+                try {
+                    startActivityForResult(intent, REQUEST_PERMISSION_CODE);
+                }catch (Exception e){
+                    e.printStackTrace();
+                    Toast.makeText(MainActivity.this, ""+e.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        translateBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                translateTV.setVisibility(View.VISIBLE);
+                translateTV.setText("");
+                if (sourceText.getText().toString().isEmpty()) {
+                    Toast.makeText(MainActivity.this, "Proszę wpisać tekst do przetłumaczenia", Toast.LENGTH_SHORT).show();
+                } else if (fromLanguageCode == 0) {
+                    Toast.makeText(MainActivity.this, "Wybierz język", Toast.LENGTH_SHORT).show();
+                } else if (toLanguageCode == 0) {
+                    Toast.makeText(MainActivity.this, "Proszę wybrać język do tłumaczenia", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+    private void translateText(int fromLanguageCode, int toLanguageCode, String source) {
+        translateTV.setText("Pobieranie modelu, proszę czekać...");
+        TranslatorOptions options = new TranslatorOptions.Builder()
+                .setSourceLanguage(fromLanguageCode)
+                .setTargetLanguage(toLanguageCode)
+                .build();
+        Translator translator = Translation.getClient(options);
+        DownloadConditions conditions = new DownloadConditions.Builder().requireWifi().build();
+        translator.downloadModelIfNeeded(conditions).addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+                translateTV.setText("Tłumaczę...");
+                translator.translate(source).addOnSuccessListener(new OnSuccessListener<String>() {
+                    @Override
+                    public void onSuccess(String s) {
+                        translateTV.setText(s);
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(MainActivity.this, "Nie udało się przetłumaczyć! Spróbuj ponownie", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(MainActivity.this, "Nie udało się pobrać modelu!! Sprawdź swoje połączenie z internetem", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_PERMISSION_CODE){
+            ArrayList<String> result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+        }
+    }
+
+    private int getLanguageCode(String language) {
+        int languageCode = 0;
+        switch (language) {
+            case "Angielski":
+                languageCode = TranslateLanguage.ENGLISH;
+                break;
+            case "Polski":
+                languageCode = TranslateLanguage.POLISH;
+                break;
+            default:
+                languageCode = 0;
+        }
+        return languageCode;
+    }
+}
